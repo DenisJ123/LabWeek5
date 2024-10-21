12650d5643afcf0cc310db8a7a5877f6a89a93c6
denis <G00403126@atu.ie>
2024-10-21 20:39:39 +0100

    Exercise 1

---
+++ src/main/java/ie/atu/week5/OrderService.java
@@ -0,0 +1,22 @@
+package ie.atu.week5;
+
+import ie.atu.week5.model.Order;
+import ie.atu.week5.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public void deleteOrder(String orderId) {
+        orderRepository.deleteById(orderId);
+    }
+}
